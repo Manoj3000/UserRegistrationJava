42612a512ee17bd8ff56fbb22d9f9571bc45e293
manoj gaikwad <gaikwadm853@gmail.com>
2021-10-05 10:16:32 +0530

    Added: password rule 1 - minimum 8 characters

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -8,7 +8,7 @@ import utility.UtilValidation;
 public class UserRegistration {
 
 	public static void main(String[] args) {
-		String firstName, lastName, email, mobileNumber;
+		String firstName, lastName, email, mobileNumber, password;
 
 		System.out.println("Welcome to User Registration program");
 		
@@ -21,24 +21,35 @@ public class UserRegistration {
 		}
 		
 		System.out.println("Enter Last Name : ");
-		lastName = sc.next();
+		Scanner sc1 = new Scanner(System.in);
+		lastName = sc1.next();
 		
 		if(!UtilValidation.checkRegex(lastName, UtilRegex.name)) {
 			System.out.println("Please enter a valid name.");
 		}
 		
 		System.out.println("Enter email : ");
-		email = sc.next();
+		Scanner sc2 = new Scanner(System.in);
+		email = sc2.next();
 		
 		if(!UtilValidation.checkRegex(email, UtilRegex.email)) {
 			System.out.println("Please enter a email.");
 		}
 		
 		System.out.println("Enter mobile number : ");
-		mobileNumber = sc.nextLine();
+		Scanner sc3 = new Scanner(System.in);
+		mobileNumber = sc3.nextLine();
 		
 		if(!UtilValidation.checkRegex(mobileNumber, UtilRegex.mobileNumber)) {
 			System.out.println("Please enter a valid mobile number.");
+		}
+		
+		System.out.println("Enter password : ");
+		Scanner sc4 = new Scanner(System.in);
+		password = sc4.next();
+		
+		if(!UtilValidation.checkRegex(password, UtilRegex.password)) {
+			System.out.println("Please enter a valid password.");
 		}
 	}
 }

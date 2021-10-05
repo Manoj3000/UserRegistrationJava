a51188dc8febbbe31f7b6a975e4992fcaa38928e
manoj gaikwad <gaikwadm853@gmail.com>
2021-10-05 10:26:54 +0530

    Added: password rule2 - minimum 1 upper case

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -9,41 +9,41 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 		String firstName, lastName, email, mobileNumber, password;
-
-		System.out.println("Welcome to User Registration program");
-		
-		System.out.println("Enter First Name : ");
-		Scanner sc = new Scanner(System.in);
-		firstName = sc.next();
-		
-		if(!UtilValidation.checkRegex(firstName, UtilRegex.name)) {
-			System.out.println("Please enter a valid name.");
-		}
-		
-		System.out.println("Enter Last Name : ");
-		Scanner sc1 = new Scanner(System.in);
-		lastName = sc1.next();
-		
-		if(!UtilValidation.checkRegex(lastName, UtilRegex.name)) {
-			System.out.println("Please enter a valid name.");
-		}
-		
-		System.out.println("Enter email : ");
-		Scanner sc2 = new Scanner(System.in);
-		email = sc2.next();
-		
-		if(!UtilValidation.checkRegex(email, UtilRegex.email)) {
-			System.out.println("Please enter a email.");
-		}
-		
-		System.out.println("Enter mobile number : ");
-		Scanner sc3 = new Scanner(System.in);
-		mobileNumber = sc3.nextLine();
-		
-		if(!UtilValidation.checkRegex(mobileNumber, UtilRegex.mobileNumber)) {
-			System.out.println("Please enter a valid mobile number.");
-		}
-		
+//
+//		System.out.println("Welcome to User Registration program");
+//		
+//		System.out.println("Enter First Name : ");
+//		Scanner sc = new Scanner(System.in);
+//		firstName = sc.next();
+//		
+//		if(!UtilValidation.checkRegex(firstName, UtilRegex.name)) {
+//			System.out.println("Please enter a valid name.");
+//		}
+//		
+//		System.out.println("Enter Last Name : ");
+//		Scanner sc1 = new Scanner(System.in);
+//		lastName = sc1.next();
+//		
+//		if(!UtilValidation.checkRegex(lastName, UtilRegex.name)) {
+//			System.out.println("Please enter a valid name.");
+//		}
+//		
+//		System.out.println("Enter email : ");
+//		Scanner sc2 = new Scanner(System.in);
+//		email = sc2.next();
+//		
+//		if(!UtilValidation.checkRegex(email, UtilRegex.email)) {
+//			System.out.println("Please enter a email.");
+//		}
+//		
+//		System.out.println("Enter mobile number : ");
+//		Scanner sc3 = new Scanner(System.in);
+//		mobileNumber = sc3.nextLine();
+//		
+//		if(!UtilValidation.checkRegex(mobileNumber, UtilRegex.mobileNumber)) {
+//			System.out.println("Please enter a valid mobile number.");
+//		}
+//		
 		System.out.println("Enter password : ");
 		Scanner sc4 = new Scanner(System.in);
 		password = sc4.next();
